bc1bea29e64125d660fd010d6ea3fd1c36e9e407
Teemu Pyykölä <tpyykola@iki.fi>
2017-06-13 19:15:05 +0300

    added checking for internal and external depencies to parse_ruby_project.py

--- parse_ruby_project.py
+++ parse_ruby_project.py
@@ -6,10 +6,12 @@ from stat import *
 #print(sys.argv)
 
 fileList = []
+pathList = []
 requirementsList = []
 
 def checkFiles(path):
 	global fileList
+	global pathList
 	global requirementsList
 
 	for file in os.listdir(path):
@@ -20,9 +22,13 @@ def checkFiles(path):
 			checkFiles(newPath)
 		else:
 			filename, extension = os.path.splitext(file)
-			if(extension == ".rb"):
-				pathAndFile = [newPath.strip(sys.argv[1]), file] 
-				fileList.append(pathAndFile)
+			if(extension == ".rb"): #TODO: tarkista että on tekstitiedosto
+				#pathAndFile = [newPath.strip(sys.argv[1]), file] 
+				#fileList.append(pathAndFile)
+
+				#pathList.append(newPath.strip(sys.argv[1]))
+				pathList.append(newPath)
+				fileList.append(os.path.join(path,file))
 				#print(file)
 				with open(newPath, "r") as rubyFile:
 					data = rubyFile.readlines()
@@ -36,13 +42,36 @@ def checkFiles(path):
 						requiredFile = line.strip((needle+" "))
 						# poistetaan rivin vaihto ja hipsut
 						requiredFile = requiredFile[1:-2]
-						requirementsList.append(requiredFile)	
-						resultLine = newPath.strip(sys.argv[1]) + ":" + requiredFile
-						#print(resultLine)
+						requirementsList.append(requiredFile)
 						
 	
 
 
 checkFiles(sys.argv[1])
 
-#print(requirementsList)
+#print(fileList)
+#['../test/omniauth-twitter/lib/omniauth-twitter/version.rb', '../test/omniauth-twitter/lib/omniauth-twitter.rb', '../test/omniauth-twitter/lib/omniauth/strategies/twitter.rb', '../test/omniauth-twitter/spec/spec_helper.rb', '../test/omniauth-twitter/spec/omniauth/strategies/twitter_spec.rb']
+
+#print(pathList)
+#['../test/omniauth-twitter/lib/omniauth-twitter/version.rb', '../test/omniauth-twitter/lib/omniauth-twitter.rb', '../test/omniauth-twitter/lib/omniauth/strategies/twitter.rb', '../test/omniauth-twitter/spec/spec_helper.rb', '../test/omniauth-twitter/spec/omniauth/strategies/twitter_spec.rb']
+
+
+print(requirementsList)
+#['omniauth-twitter/version', 'omniauth/strategies/twitter', 'omniauth-oauth', 'json', 'simplecov', 'rspec', 'rack/test', 'webmock/rspec', 'omniauth', 'omniauth-twitter', 'spec_helper']
+
+
+
+for requirement in requirementsList:
+	value = requirement+".rb"
+	#print(value)
+	i=0
+	index = -1
+	for file in fileList:
+		if(value in file):
+			index = i
+			
+	if(index == -1):
+		print(requirement + " ulkoinen")
+	else:
+		resultLine = requirement + ":" + pathList[index]
+		print(resultLine)
